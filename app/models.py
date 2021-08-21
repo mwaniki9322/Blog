9bba65841b85407b2cf3c6cd980829d0df3294ed
Dennis Mwaniki <damos5776@gmail.com>
2021-08-21 20:47:17 +0300

    downvote

--- app/models.py
+++ app/models.py
@@ -1,11 +1,8 @@
-
-from operator import index
-
-from sqlalchemy.orm import backref
 from . import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -39,6 +36,14 @@ class User(db.Model,UserMixin):
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)
 
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
     def __repr__(self):
         return f'User {self.username}'
 
@@ -52,6 +57,8 @@ class Blog(db.Model):
     comment = db.relationship('Comment',backref='blog',lazy='dynamic')
     upvote=db.relationship('Upvote',backref='blog',lazy='dynamic')
     downvote=db.relationship('Downvote',backref='blog',lazy='dynamic')
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    category = db.Column(db.String(255), index = True,nullable = False)
 
 
     def save_blog(self):
