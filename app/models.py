ffbef2678ea888973f69f26ccba6b94cae62cdb5
Dennis Mwaniki <damos5776@gmail.com>
2021-08-21 09:15:03 +0300

    user authentication

--- app/models.py
+++ app/models.py
@@ -1,9 +1,15 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pass_secure=db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot access password attribute')
 
     def __repr__(self):
         return f'User {self.username}'
