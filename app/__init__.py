b723ff5454106dfc797d229fe8a060f5202e11a9
Dennis Mwaniki <damos5776@gmail.com>
2021-08-21 08:48:44 +0300

    index view function

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app(config_name):
  bootstrap.init_app(app)
 
  #registering blueprint
- from main import main as main_blueprint
+ from .main import main as main_blueprint
  app.register_blueprint(main_blueprint)
 
     # Will add the views and forms
---
+++ start.sh
@@ -0,0 +1,4 @@
+
+export SECRET_KEY='mwaniki'
+
+python3.9 manage.py server
