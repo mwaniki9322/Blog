0e402a7ed1bc861e08083fe1cc18fc408ba5c56a
Dennis Mwaniki <damos5776@gmail.com>
2021-08-21 08:35:46 +0300

    app instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,24 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap=Bootstrap()
 
 
-app=Flask(__name__)
+def create_app(config_name):
+
+ app=Flask(__name__)
+
+ #creating app configurations
+ app.config.from_object(config_options[config_name])
+
+ # Initializing flask extensions
+ bootstrap.init_app(app)
+
+ #registering blueprint
+ from main import main as main_blueprint
+ app.register_blueprint(main_blueprint)
+
+    # Will add the views and forms
+
+ return app
